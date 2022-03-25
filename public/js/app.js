e9c107a85014482c528c012f0c7a5f35345623d0
gabriel <gamcode98@gmail.com>
2022-03-25 15:29:04 -0300

    ready I guess

--- public/js/app.js
+++ public/js/app.js
@@ -4,96 +4,105 @@ const $main = d.querySelector("main");
 const $xhr = d.getElementById("xhr");
 const $fragment = d.createDocumentFragment();
 const $reportsToChange = d.querySelectorAll(".report-change");
-let timeframe;
+let timeframe, json;
 
-// console.log($reportsToChange[0].textContent.toUpperCase().trim());
+xhr.addEventListener("readystatechange", (e) => {
+  if (xhr.readyState !== 4) return;
 
-const oldMethod = (time) => {
-  xhr.addEventListener("readystatechange", (e) => {
-    if (xhr.readyState !== 4) return;
+  if (xhr.status >= 200 && xhr.status < 300) {
+    json = JSON.parse(xhr.responseText);
 
-    if (xhr.status >= 200 && xhr.status < 300) {
-      let json = JSON.parse(xhr.responseText);
+    json.forEach((el) => {
+      //Creating elements and seetings properties
+      const $section = d.createElement("section");
 
-      json.forEach((el) => {
-        //Creating elements and seetings properties
-        const $section = d.createElement("section");
+      $section.classList.add("report", el.title.replace(/ /g, ""));
+      const $header = d.createElement("header");
+      $header.classList.add("report-to");
+      const $h2 = d.createElement("h2");
+      $h2.textContent = el.title;
+      const $img = d.createElement("img");
+      $img.classList.add("icon-ellipsis");
+      $img.setAttribute("src", "./assets/images/icon-ellipsis.svg");
+      $img.setAttribute("alt", "icon-ellipsis");
+      const $div = d.createElement("div");
+      $div.classList.add("time");
+      const $p1 = d.createElement("p");
+      $p1.classList.add("time-mode");
 
-        $section.classList.add("report", el.title.replace(/ /g, ""));
-        const $header = d.createElement("header");
-        $header.classList.add("report-to");
-        const $h2 = d.createElement("h2");
-        $h2.textContent = el.title;
-        const $img = d.createElement("img");
-        $img.classList.add("icon-ellipsis");
-        $img.setAttribute("src", "./assets/images/icon-ellipsis.svg");
-        $img.setAttribute("alt", "icon-ellipsis");
-        const $div = d.createElement("div");
-        $div.classList.add("time");
-        const $p1 = d.createElement("p");
-        $p1.classList.add("time-mode");
+      const $p2 = d.createElement("p");
+      $p2.classList.add("last-time");
 
-        const $p2 = d.createElement("p");
-        $p2.classList.add("last-time");
+      $p1.textContent = `${el.timeframes.weekly.current}hrs`;
+      $p2.textContent = `Last Week - ${el.timeframes.weekly.previous}hrs`;
 
-        if (time === "weekly") {
-          $p1.textContent = `${el.timeframes.weekly.current}hrs`;
-          $p2.textContent = `Last Week - ${el.timeframes.weekly.previous}hrs`;
-          console.log("hola", time);
-        }
-        if (time === "daily") {
-          $p1.textContent = `${el.timeframes.daily.current}hrs`;
-          $p2.textContent = `Last Day - ${el.timeframes.daily.previous}hrs`;
-          console.log("hola", time);
-        }
-        if (time === "monthly") {
-          $p1.textContent = `${el.timeframes.monthly.current}hrs`;
-          $p2.textContent = `Last Month - ${el.timeframes.monthly.previous}hrs`;
-          console.log("hola", time);
-        }
+      //Adding elements
+      $header.appendChild($h2);
+      $header.appendChild($img);
+      $div.appendChild($p1);
+      $div.appendChild($p2);
+      $section.appendChild($header);
+      $section.appendChild($div);
 
-        //Adding elements
-        $header.appendChild($h2);
-        $header.appendChild($img);
-        $div.appendChild($p1);
-        $div.appendChild($p2);
-        $section.appendChild($header);
-        $section.appendChild($div);
-
-        $fragment.appendChild($section);
-      });
-      $main.appendChild($fragment);
-      console.log("Agregando al fragmento");
-    } else {
-      //console.log("error");
-      let message = xhr.statusText || "Ocurrió un error";
-      $xhr.innerHTML = `Error ${xhr.status}: ${message}`;
-    }
-  });
-
-  xhr.open("GET", "./assets/data.json");
-
-  xhr.send();
-
-  console.log("estoy aqui de nuevo");
-};
-
-$reportsToChange.forEach((el) => {
-  el.addEventListener("click", () => {
-    $reportsToChange.forEach((subEl) => {
-      subEl.classList.remove("isActive");
+      $fragment.appendChild($section);
     });
+    $main.appendChild($fragment);
+  } else {
+    let message = xhr.statusText || "Ocurrió un error";
+    $xhr.innerHTML = `Error ${xhr.status}: ${message}`;
+  }
 
-    el.classList.add("isActive");
-    timeframe = el.textContent.toLowerCase().trim();
-    console.log(timeframe);
-    oldMethod(timeframe);
+  const changingState = (time) => {
+    let i = 0;
+    if (time === "weekly") {
+      json.forEach((el) => {
+        d.querySelectorAll(".time-mode")[
+          i
+        ].textContent = `${el.timeframes.weekly.current}hrs`;
+        d.querySelectorAll(".last-time")[
+          i
+        ].textContent = `Last Week - ${el.timeframes.weekly.previous}hrs`;
+        i++;
+      });
+    }
+    if (time === "daily") {
+      json.forEach((el) => {
+        d.querySelectorAll(".time-mode")[
+          i
+        ].textContent = `${el.timeframes.daily.current}hrs`;
+        d.querySelectorAll(".last-time")[
+          i
+        ].textContent = `Last Day - ${el.timeframes.daily.previous}hrs`;
+        i++;
+      });
+    }
+    if (time === "monthly") {
+      json.forEach((el) => {
+        d.querySelectorAll(".time-mode")[
+          i
+        ].textContent = `${el.timeframes.monthly.current}hrs`;
+        d.querySelectorAll(".last-time")[
+          i
+        ].textContent = `Last Month - ${el.timeframes.monthly.previous}hrs`;
+        i++;
+      });
+    }
+  };
+
+  $reportsToChange.forEach((el) => {
+    el.addEventListener("click", (e) => {
+      e.preventDefault();
+      $reportsToChange.forEach((subEl) => {
+        subEl.classList.remove("isActive");
+      });
+
+      el.classList.add("isActive");
+      timeframe = el.textContent.toLowerCase().trim();
+      changingState(timeframe);
+    });
   });
 });
 
-// $reportsToChange.forEach((el) => {
-//   if (el.classList.contains("isActive")) {
-//     timeframe = el.textContent.toLowerCase().trim();
-//     console.log(timeframe);
-//   }
-// });
+xhr.open("GET", "./assets/data.json");
+
+xhr.send();
